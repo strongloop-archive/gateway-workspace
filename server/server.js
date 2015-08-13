bd794ccb16ac8d0480b507a17df5e37b25659d57
Raymond Feng <raymond@strongloop.com>
2015-08-13 08:25:56 -0700

    Change the main script so that server.js can be used for booting

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
-/*!
- * Expose the module as a LoopBack component
- * @type {exports|module.exports}
+/**
+ * Allow gateway-workspace to be booted so that Angular SDK code generation
+ * can work with strong-arc.
+ *
+ * Please note loopback-workspace is dev dependency. Using this code outside
+ * of strong-arc will fail.
  */
 var boot = require('loopback-boot');
+var app = require('loopback-workspace');
+boot(app, __dirname);
 
-module.exports = function(app, options) {
-  boot(app, __dirname);
-};
+module.exports = app;
+
+if (require.main === module) {
+  app.start();
+}
+
